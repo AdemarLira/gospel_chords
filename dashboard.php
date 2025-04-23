7af6786aa9b2ca4eb775abc2f3e819fc63befc31
AdemarLira <ademarliraneto@gmail.com>
2025-04-22 22:06:50 -0300

    ajustes lo layout dao dashboard

--- dashboard.php
+++ dashboard.php
@@ -4,33 +4,54 @@ include_once("php/conexao.php");
 
 <!DOCTYPE html>
 <html lang="pt-br">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <script src="https://kit.fontawesome.com/328073035f.js" crossorigin="anonymous"></script>
-  <link rel="stylesheet" href="css/dashboard1.css">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/328073035f.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/dashboard1.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
   <title>Página inicial</title>
 </head>
 <body>
-  <header class="header">
-    <div>
-      <div class="header-pesquisa">
-        <form action="" class="header-pesquisa1" id="header-form">
-          <img src="img/logo.png" id="logo-gospelChord">
-          <label for="js-header-pesquisa" class="hidden-text"></label>
-          <input name="pesquisar" id="js-header-pesquisa" type="text" placeholder="Qual música você deseja?">
-          
-          <!-- Botão de busca com ícone -->
-          <button type="submit" class="header-botao-pesquisa"><i class="fa-solid fa-magnifying-glass fa-sm"></i></button>
-          <button class="header-botao-premium">Premium <i class="fa-regular fa-star"></i></button>
-        </form>
-      </div>
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+  <nav class="navbar bg-body-tertiary">
+    <div class="container">
+      <a class="navbar-brand" href="index.html">
+        <img src="img/logo.png" id="logo-gospelChord" alt="logo" height="40">
+      </a>
     </div>
-  </header>
-  <div id="background">
-    <video loop autoplay muted>
-      <source src="mp4/violao.mp4" type="video/mp4">
-    </video>
+  </nav>
+    <img src=>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="nav nav-underline">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Active</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+          </li>
+        </ul>
+      <button class="header-botao-premium">Premium <i class="bi bi-bookmark-star-fill"></i></button>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Qual música você deseja?" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit"><i class="fa-solid fa-magnifying-glass fa-sm"></i></button>
+      </form>
+    </div>
   </div>
+</nav>
+<div id="background">
+  <video loop autoplay muted>
+    <source src="mp4/violao.mp4" type="video/mp4">
+  </video>
+</div>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
